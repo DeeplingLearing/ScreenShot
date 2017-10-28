0b83832e4a745ed84b1691a7fbf91795e8c0b23d
niecong <729951956@qq.com>
2017-10-29 01:20:56 +0800

    添加截图过程的辅助信息和背景色透明度改变

--- screenshot/screenshot/screenshotDlg.cpp
+++ screenshot/screenshot/screenshotDlg.cpp
@@ -22,7 +22,12 @@ CscreenshotDlg::CscreenshotDlg(CWnd* pParent /*=NULL*/)
 	, m_cyScreen(0)
 	, m_xScreen(0)
 	, m_yScreen(0)
+	, m_bMasked(false)
+	, m_ptCur(0)
+	, m_nAlph(255)
+	, m_bPaintOther(true)
 {
+	m_pBmp = NULL;
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
@@ -34,8 +39,19 @@ void CscreenshotDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CscreenshotDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_MOUSEMOVE()
+//	ON_WM_ERASEBKGND()
+ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
 
+CscreenshotDlg::~CscreenshotDlg()
+{
+	if (m_pBmp)
+	{
+		delete m_pBmp;
+		m_pBmp = NULL;
+	}
+}
 
 // CscreenshotDlg 消息处理程序
 
@@ -54,9 +70,13 @@ BOOL CscreenshotDlg::OnInitDialog()
 	m_cyScreen = ::GetSystemMetrics(SM_CYVIRTUALSCREEN);
 	m_xScreen = ::GetSystemMetrics(SM_XVIRTUALSCREEN);
 	m_yScreen = ::GetSystemMetrics(SM_YVIRTUALSCREEN);
+	//抓取屏幕位图
+	CaputerImage();
 	//初始化窗口为pop
 	SetWindowLong(m_hWnd, GWL_STYLE, (LONG)WS_POPUP);
-	SetWindowPos(&wndTopMost, m_xScreen, m_yScreen, m_cxScreen, m_cyScreen, SWP_SHOWWINDOW);
+	//鼠标隐藏
+	//ShowCursor(FALSE);
+	SetWindowPos(NULL, m_xScreen, m_yScreen, m_cxScreen, m_cyScreen, SWP_SHOWWINDOW);
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -86,7 +106,26 @@ void CscreenshotDlg::OnPaint()
 	}
 	else
 	{
-		CDialogEx::OnPaint();
+		//CDialogEx::OnPaint();
+		//双缓冲
+		CDC *pDc = GetDC();
+		CDC dcMem;
+		dcMem.CreateCompatibleDC(pDc);
+		CBitmap bmp;
+		bmp.CreateCompatibleBitmap(pDc, m_cxScreen, m_cyScreen);
+		CBitmap *pOldbmp = dcMem.SelectObject(&bmp);
+		OnPaintBackgroundImage(&dcMem);
+		if (m_bPaintOther) //是否需要绘制辅助信息
+		{
+			DoPaintLine(&dcMem);
+			//DoPaintTip(&dcMem);
+		}
+		
+
+		pDc->BitBlt(m_xScreen, m_yScreen, m_cxScreen, m_cyScreen, &dcMem, 0, 0, SRCCOPY);
+
+		DeleteDC(dcMem);
+		ReleaseDC(pDc);
 	}
 }
 
@@ -97,3 +136,168 @@ HCURSOR CscreenshotDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+// 抓取图片
+BOOL CscreenshotDlg::CaputerImage()
+{
+	//屏幕设备环境
+	HDC hScreenDc = ::GetDC(::GetDesktopWindow());
+	if (NULL == hScreenDc)
+	{
+		MessageBox(_T("获取桌面设备环境失败!"));
+	}
+	CDC dc;
+	dc.Attach(hScreenDc);
+	//窗口设备环境
+	CDC *pDc = GetDC();
+	CDC dcMem;
+	dcMem.CreateCompatibleDC(pDc);
+	m_pBmp = new CBitmap;
+	m_pBmp->CreateCompatibleBitmap(pDc, m_cxScreen, m_cyScreen);
+	dcMem.SelectObject(m_pBmp);
+	//拷贝屏幕位图到内存设备环境中
+	if (!dcMem.BitBlt(m_xScreen, m_yScreen, m_cxScreen, m_cyScreen, &dc, 0, 0, SRCCOPY))
+	{
+		MessageBox(_T("抓取屏幕位图失败!"), _T("ERROR"), MB_ICONERROR);
+	}
+
+	//释放资源
+	DeleteDC(dcMem);
+	ReleaseDC(pDc);
+	DeleteDC(dc);
+	::ReleaseDC(NULL, hScreenDc);
+	return TRUE;
+}
+
+
+// 绘制背景图片
+BOOL CscreenshotDlg::OnPaintBackgroundImage(CDC * pDc)
+{
+	CDC *pTempDc = GetDC();
+	CDC tempDcMem;
+	tempDcMem.CreateCompatibleDC(pTempDc);
+	CBitmap *pbmp = tempDcMem.SelectObject(m_pBmp);
+
+	if (false == m_bMasked)
+	{
+		//设置位图透明度
+		//先拷贝到内存设备环境中,一起绘制到窗口设备环境中
+		//**需要改变背景透明度时,先将该修改好的位图存入兼容设备环境中
+		BLENDFUNCTION ftn;
+		ftn.BlendOp = AC_SRC_OVER;
+		ftn.BlendFlags = 0;
+		ftn.SourceConstantAlpha = m_nAlph; //透明度值
+		ftn.AlphaFormat = 0;
+		if (!AlphaBlend(pDc->GetSafeHdc(), m_xScreen, m_yScreen, m_cxScreen, m_cyScreen,
+			tempDcMem.GetSafeHdc(), 0, 0, m_cxScreen, m_cyScreen, ftn)
+			)
+		{
+			MessageBox(_T("设置背景透明度失败!"), _T("WARING"), MB_ICONWARNING);
+		}
+	}
+	else
+	{
+		if (!pDc->BitBlt(m_xScreen, m_yScreen, m_cxScreen, m_cyScreen, &tempDcMem, 0, 0, SRCCOPY))
+		{
+			MessageBox(_T("绘制背景失败!"), _T("WARING"), MB_ICONWARNING);
+		}
+	}
+	//释放资源
+	tempDcMem.SelectObject(pbmp);
+	DeleteDC(tempDcMem);
+	ReleaseDC(pTempDc);
+	DeleteObject(pbmp);
+
+	return TRUE;
+}
+
+
+// 绘制坐标线
+BOOL CscreenshotDlg::DoPaintLine(CDC * pDc)
+{
+	CPen pen(PS_SOLID, 1, RGB(0, 0, 0)); //黑色画笔
+	CPen *poldPen = pDc->SelectObject(&pen);
+
+	//x坐标
+	pDc->MoveTo(0, m_ptCur.y);
+	pDc->LineTo(m_cxScreen, m_ptCur.y);
+	//y坐标
+	pDc->MoveTo(m_ptCur.x, 0);
+	pDc->LineTo(m_ptCur.x, m_cyScreen);
+
+	//释放资源
+	pDc->SelectObject(poldPen);
+	DeleteObject(poldPen);
+	DeleteObject(pen);
+
+	return TRUE;
+}
+
+//鼠标移动时，提示坐标线也会移动
+void CscreenshotDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO:  在此添加消息处理程序代码和/或调用默认值
+	m_ptCur = point;
+	//InvalidateRect(NULL);
+	//PostMessage(WM_PAINT);
+	UpdateWindow();
+	//CDialogEx::OnMouseMove(nFlags, point);
+}
+
+
+//BOOL CscreenshotDlg::OnEraseBkgnd(CDC* pDC)
+//{
+//	// TODO:  在此添加消息处理程序代码和/或调用默认值
+//	return 0;
+//	//return CDialogEx::OnEraseBkgnd(pDC);
+//}
+
+
+// 绘制提示框
+BOOL CscreenshotDlg::DoPaintTip(CDC * pDc)
+{
+	//绘制背景为灰色的矩形提示框
+	CBrush brush;
+	brush.CreateSolidBrush(RGB(100, 100, 100));
+	CBrush *oldBrush = pDc->SelectObject(&brush);
+	//矩形的宽高
+	int cx = 200;
+	int cy = 50;
+	pDc->Rectangle(m_ptCur.x, m_ptCur.y, m_ptCur.x + cx, m_ptCur.y + cy);
+	//在提示框内绘制提示文本信息
+	CPen pen(PS_SOLID, 1, RGB(255, 255, 255));
+	CPen *oldPen = pDc->SelectObject(&pen);
+	CString strTip = _T("鼠标左键选择截图区域");
+	CRect rect;
+	rect.left = m_ptCur.x + 5;
+	rect.top = m_ptCur.y + 5;
+	rect.right = rect.left + cx;
+	rect.bottom = rect.top + cy;
+	SetBkMode(pDc->GetSafeHdc(), TRANSPARENT);
+	SetBkColor(pDc->GetSafeHdc(), RGB(255, 255, 255));
+	pDc->DrawText(strTip, rect, DT_VCENTER | DT_LEFT);
+
+	//释放资源
+	pDc->SelectObject(oldPen);
+	pDc->SelectObject(oldBrush);
+	//DeleteObject(pen);
+	DeleteObject(oldPen);
+	DeleteObject(brush);
+	DeleteObject(oldBrush);
+
+	return TRUE;
+}
+
+//鼠标左键点击, 改变透明度并且开始绘制需要截取的区域
+void CscreenshotDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO:  在此添加消息处理程序代码和/或调用默认值
+	//改变背景透明度
+	m_nAlph = 225;
+	//辅助信息撤销
+	m_bPaintOther = false;
+	//
+	UpdateWindow();
+	//CDialogEx::OnLButtonDown(nFlags, point);
+}
--- screenshot/screenshot/screenshotDlg.h
+++ screenshot/screenshot/screenshotDlg.h
@@ -3,6 +3,8 @@
 //
 
 #pragma once
+#include "afxwin.h"
+#include "atltypes.h"
 
 
 // CscreenshotDlg 对话框
@@ -11,7 +13,7 @@ class CscreenshotDlg : public CDialogEx
 // 构造
 public:
 	CscreenshotDlg(CWnd* pParent = NULL);	// 标准构造函数
-
+	virtual ~CscreenshotDlg();         //析构函数
 // 对话框数据
 	enum { IDD = IDD_SCREENSHOT_DIALOG };
 
@@ -37,4 +39,31 @@ private:
 	int m_xScreen;
 	// 屏幕做上角y坐标
 	int m_yScreen;
+	// 背景图片
+	CBitmap *m_pBmp;
+protected:
+	// 抓取图片
+	BOOL CaputerImage();
+public:
+	// 绘制背景图片
+	BOOL OnPaintBackgroundImage(CDC * pDc);
+private:
+	// 是否绘制透明背景色
+	bool m_bMasked;
+	// 鼠标坐标
+	CPoint m_ptCur;
+public:
+	// 绘制坐标线
+	BOOL DoPaintLine(CDC * pDc);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	// 绘制提示框	
+	BOOL DoPaintTip(CDC * pDc);
+private:
+	// 透明度值
+	int m_nAlph;
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+private:
+	// 是否需要绘制辅助信息
+	bool m_bPaintOther;
 };
